2d3f4c49958001ca171b6d4cd4e3800b0588d79f
agngara <agngara@gmail.com>
2025-05-08 13:01:13 +0200

    Första commit

---
+++ SDG Admin/src/sdg/admin/SDGAdmin.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sdg.admin;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
+/**
+ *
+ * @author agnesgaramvolgyi
+ */
+public class SDGAdmin {
+    private static InfDB idb;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        try {
+            idb = new InfDB("sdgsweden", "3306", "dbAdmin2024", "dbAdmin2024PW");
+
+        } catch (InfException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+}
