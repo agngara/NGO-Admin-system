d89eb06aa51a76e6d624a69fa4b632809595a881
01010100heo <wikstromtheo@gmail.com>
2025-05-15 14:39:17 +0200

    Omstrukturering och ny klass för dbuppkoppling
    Jag har skapat en javaklass som har en klassmetod som när den kallas returnerar ett InfDB objekt som innehåller uppkopplingsuppgifter till databasen. Så om man behöver prata med databasen i sin fil, kan man skapa en variabel av typen InfDB som ett fält och i konstruktorn ge den värdet av metoden databaseConnection, alltså ex:  InfDB idb = DatabaseInterface.databaseConnection()
    Glöm inte att du måste även importera oru.inf.InfDB för att kunna skapa en variabel av objekttypen InfDB :)

---
+++ SDG Admin/src/OrgEntities/Anstalld.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package OrgEntities;
+import java.util.HashMap;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+/**
+ *
+ * @author theow
+ */
+
+
+
+/* public class Anstalld {
+    
+    private HashMap<String, String> anstalld; 
+    private InfDB idb;
+    private String query;
+    
+    public Anstalld(InfDB idb) {
+    
+    this.idb = idb;
+    anstalld = idb.fetchRow(query)
+        
+        
+    }
+    
+    
+}*/
