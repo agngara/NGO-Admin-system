56d18cc23c5a19c03f1424a6d02ea28a3f943361
agngara <agngara@gmail.com>
2025-06-04 14:43:40 +0200

    fixxxx

--- SDG Admin/src/orgEntities/Avdelning.java
+++ SDG Admin/src/orgEntities/Avdelning.java
@@ -25,6 +25,9 @@ public class Avdelning {
     private AvdelningHanterare avdelningHanterare; 
     
     
+    
+    
+    
     public Avdelning (AvdelningHanterare avdelningHanterare)
     {
         
